bba5cdcf6af9e552f39c22aab8eccb7ba2483b72
Sergy <qwang@myuax.com>
2026-03-03 09:24:44 +0100

    Chore: Clase Entrenador creada

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -11,4 +11,80 @@
  * @version 1.0
  */
 public class Entrenador {
+    private int id;
+    private String nombre;
+    private String especialidad;
+    private int yearsExperiencia;
+    private int idAlt = 100;
+
+    /**
+     * Constructor de la clase Entrenador
+     * @param id identificador
+     * @param nombre nombre del entrenador
+     * @param especialidad especializacion del entrenador
+     * @param yearsExperiencia años de experiencia del entrenador
+     */
+    public Entrenador (int id, String nombre, String especialidad, int yearsExperiencia) {
+        if (id > 0) {
+            this.id = id;
+        } else {
+            this.id = idAlt;
+            idAlt++;
+        }
+
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.yearsExperiencia = yearsExperiencia;
+    }
+
+    /**
+     * Devuelve identificador de Entrenador
+     * @return int id de clase Entrenador
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Devuelve nombre del Entrenador
+     * @return String nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Devuelve la especialidad del Entrenador
+     * @return String especialidad
+     */
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    /**
+     * Devuelve los años de experiencia del Entrenador
+     * @return int yearsExperiencia
+     */
+    public int getYearsExperiencia() {
+        return yearsExperiencia;
+    }
+
+    /**
+     * Metodo toString de Entrenador
+     * @return String del entrenador con sus atributos
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Entrenador{");
+
+        sb.append(String.format("id = %d", id));
+        sb.append(String.format("; nombre = %s", nombre));
+        sb.append(String.format("; especialidad = %s", especialidad));
+        sb.append(String.format("; yearsExperiencia = %d", yearsExperiencia));
+        sb.append("}");
+
+        return sb.toString();
+    }
+
+
 }
