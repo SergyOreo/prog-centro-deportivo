fe492f434d41a375b30a9ed64b68d62b06f8c4cf
Sergy <qwang@myuax.com>
2026-03-03 10:12:12 +0100

    test: Primera prueba en el main añadida

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        CentroDeportivo centroDeportivo;
+        Entrenador entrenador1;
+        Entrenador entrenador2;
+        Entrenador entrenador3;
 
+        centroDeportivo = new CentroDeportivo(15);
+
+        entrenador1 = new Entrenador(1,"Entrenador 1", "Especialidad 1", 3);
+        entrenador2 = new Entrenador(2,"Entrenador 2", "Especialidad 2", 6);
+        entrenador3 = new Entrenador(3,"Entrenador 3", "Especialidad 3", 7);
+
+        centroDeportivo.registrarEntrenador(entrenador1);
+        centroDeportivo.registrarEntrenador(entrenador2);
+        centroDeportivo.registrarEntrenador(entrenador3);
+
+        System.out.println(centroDeportivo.mostrarEntrenadores());
+        System.out.println("Total de entrenadores: " + centroDeportivo.contarEntrenadores());
+
+        System.out.println(centroDeportivo.buscarEntrenador(2));
+        System.out.println(centroDeportivo.buscarEntrenador(9));
     }
 }
